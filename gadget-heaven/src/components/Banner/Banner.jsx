e3b1edb6c8cf290882d74b005d3174b7838553bb
Yousuf007 <yousufjobaer@gmail.com>
2024-11-04 00:26:14 +0600

    added the footer section

--- gadget-heaven/src/components/Banner/Banner.jsx
+++ gadget-heaven/src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import bannerImg from '../../assets/images/banner.jpg'
 
 const Banner = props => {
     return (
-        <div className="hero bg-base-200 border-2 p-4 max-w-2xl mx-auto absolute top-[400px] ml-80 rounded-2xl">
+        <div className="hero bg-white border-2 p-4 max-w-2xl mx-auto absolute top-[400px] ml-80 rounded-2xl">
             <img className='h-[450px] rounded-2xl' src={bannerImg} alt="" />
         </div>
     );
--- gadget-heaven/src/components/Footer/Footer.jsx
+++ gadget-heaven/src/components/Footer/Footer.jsx
@@ -3,27 +3,35 @@ import PropTypes from 'prop-types';
 const Footer = props => {
     return (
         <div>
-            <footer className="footer bg-neutral text-neutral-content p-10">
-                <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </nav>
+            <footer className="bg-white p-20">
+                <div className='text-center mb-5'>
+                    <h2 className="text-4xl my-4 font-bold">Gadget Heaven</h2>
+                    <p className='text-sm opacity-70'>Leading the way in cutting-edge technology and innovation.</p>
+                </div>
+                <div className="border-t-2 border my-2 max-w-6xl mx-auto"></div>
+                <section className='max-w-4xl mx-auto grid grid-cols-3 mt-5'>
+                    <div className='text-center'>
+                        <h4 className="text-xl font-bold mb-2">Services</h4>
+                        <p className='opacity-70 text-sm'>Product Support</p>
+                        <p className='opacity-70 text-sm'> Order Tracking</p>   
+                        <p className='opacity-70 text-sm'>Shipping & Delivery</p>
+                        <p className='opacity-70 text-sm'>Returns</p>
+                    </div>
+                    <div className='text-center'>
+                        <h4 className="text-xl font-bold mb-2">Company</h4>
+                        <p className='opacity-70 text-sm'> About Us</p>
+                        <p className='opacity-70 text-sm'>Careers</p>
+                        <p className='opacity-70 text-sm'> Contact</p>
+                    </div>
+                    <div className='text-center'>
+                        <h4 className="text-xl font-bold mb-2">Legal</h4>
+                        <p className='opacity-70 text-sm'>Terms of Service</p>
+                        <p className='opacity-70 text-sm'> Privacy Policy</p>   
+                        <p className='opacity-70 text-sm'>Cookie Policy</p>
+                        <p className='opacity-70 text-sm'>Returns</p>
+                    </div>
+                </section>
+
             </footer>
         </div>
     );
